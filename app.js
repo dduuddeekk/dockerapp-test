8399162d9a87f5b2a5d3a149e9e7d1a85c386e84
dduuddeekk <jajajablablabla123@gmail.com>
2023-12-28 21:19:34 +0800

    App ready.

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+    res.send("Hi, I'm Emu Ohtori");
+});
+
+app.listen(PORT, () => {
+    console.log("Server ready ....");
+});
